3b39f835bf5d4bf31a028daa57dc873031c6f823
Hristo Hinkin <hhinkin@gmail.com>
2021-01-10 09:49:26 +0000

    initial commit

---
+++ app/Models/Team.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Jetstream\Events\TeamCreated;
+use Laravel\Jetstream\Events\TeamDeleted;
+use Laravel\Jetstream\Events\TeamUpdated;
+use Laravel\Jetstream\Team as JetstreamTeam;
+use App\Models\Traits\ClearsResponseCache;
+
+class Team extends JetstreamTeam
+{
+    use HasFactory;
+    use ClearsResponseCache;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'personal_team' => 'boolean',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'personal_team',
+    ];
+
+    /**
+     * The event map for the model.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'created' => TeamCreated::class,
+        'updated' => TeamUpdated::class,
+        'deleted' => TeamDeleted::class,
+    ];
+}
---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+require('./bootstrap');
+require('@ryangjchandler/spruce');
+require('alpine-turbo-drive-adapter');
+require('alpine-magic-helpers');
+require('alpinejs');
+require('livewire-turbolinks');
+const Turbolinks = require("turbolinks");
+Turbolinks.start();
